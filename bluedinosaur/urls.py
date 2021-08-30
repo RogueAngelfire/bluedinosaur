a711fa9ec586e86de8cd72e5d752bcb739457550
Robin Collins <info@robincollins.co.uk>
2021-08-30 20:01:30 +0100

    removed errors but links still an issue

--- bluedinosaur/urls.py
+++ bluedinosaur/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    #path('about/', include('about.urls')),
-    #path('contact/', include('contact.urls')),
+    path('about/', include('about.urls')),
+    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
