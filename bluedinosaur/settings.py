2d412b6dab591bbed4ab5ed760a1315f27004c21
Robin Collins <info@robincollins.co.uk>
2021-10-04 13:16:27 +0100

    delpoyment part 1

--- bluedinosaur/settings.py
+++ bluedinosaur/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -107,10 +108,10 @@ WSGI_APPLICATION = 'bluedinosaur.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+  }
 }
 
 
