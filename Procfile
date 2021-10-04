4cfc73efd21de8de7ec3e41099aaa3906324789f
Robin Collins <info@robincollins.co.uk>
2021-10-04 15:22:45 +0100

    Deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bluedinosaur.wsgi:application
--- bluedinosaur/settings.py
+++ bluedinosaur/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '#yv55gwq21j0^go#jtl#o9283c6iobf)e90b58rnv_&eiw=gj3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blue-dinosaur-animation.heroku.app.com', 'localhost']
 
 
 # Application definition
@@ -107,12 +107,17 @@ WSGI_APPLICATION = 'bluedinosaur.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-  }
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
