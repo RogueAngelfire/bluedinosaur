b27a416f5dfc0800fbfc842fc5c7d1b44f9ebbfd
Robin Collins <info@robincollins.co.uk>
2021-10-04 18:45:02 +0100

    deployment part 5 test to see if bucket name has the extension

--- bluedinosaur/settings.py
+++ bluedinosaur/settings.py
@@ -167,7 +167,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # S3 Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'blue-dinosaur-animation'
+    AWS_STORAGE_BUCKET_NAME = 'blue-dinosaur-animation-staticfiles-user'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
