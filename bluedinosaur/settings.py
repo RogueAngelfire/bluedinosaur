16c802e5d054f0f0ac8314d7dc1e66cde1e4a369
Robin Collins <info@robincollins.co.uk>
2021-10-04 18:34:15 +0100

    deployment part 5 corrected media url

--- bluedinosaur/settings.py
+++ bluedinosaur/settings.py
@@ -181,4 +181,4 @@ if 'USE_AWS' in os.environ:
 
     # Override static and media URLs in Production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
-    MEDIA_URL = F'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
