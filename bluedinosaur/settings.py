0b9a3c26261987b662c929242fca770659de16b5
Robin Collins <info@robincollins.co.uk>
2021-10-05 12:37:43 +0100

    Deployment 7 altered more settings adding whitenoise

--- bluedinosaur/settings.py
+++ bluedinosaur/settings.py
@@ -118,7 +118,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+            'NAME': (BASE_DIR / 'db.sqlite3'),
         }
     }
 
@@ -161,6 +161,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_FILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
