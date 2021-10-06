4beb8f653a1a248b92ce8447b6d81162ce41f498
Robin Collins <info@robincollins.co.uk>
2021-10-06 15:37:56 +0100

    Reverted Website back when Static Root failed

--- bluedinosaur/settings.py
+++ bluedinosaur/settings.py
@@ -161,8 +161,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-# Added line below to fix static file issues with AWS and Heroku
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
