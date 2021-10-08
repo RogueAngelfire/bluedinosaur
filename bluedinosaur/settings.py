63aadce047965aaed47d12d4df1dad91e206a5ab
Robin Collins <info@robincollins.co.uk>
2021-10-08 15:19:39 +0100

    updated settings

--- bluedinosaur/settings.py
+++ bluedinosaur/settings.py
@@ -112,7 +112,6 @@ WSGI_APPLICATION = 'bluedinosaur.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
